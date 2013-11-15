e80a3b5f01685cf36b4e9befadf7e5bf90b01cef
Hakeem Javaid <texel23.doc.ic.ac.uk>
2013-11-15 18:59:18 +0000

    added if conversion plus updated helper fnctions

--- src/frontend/Main.java
+++ src/frontend/Main.java
@@ -17,7 +17,7 @@ public class Main {
 	public static void main(String[] args) throws IOException{
 		// Get file name
 		//String file = args[0];
-		String file = "../wacc_examples/valid/print/print.wacc";
+		String file = "../wacc_examples/valid/if/ifTrue.wacc";
 		//System.out.println("File: " + file);
 		
 		// Read in file
