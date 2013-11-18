39e904770aca4b772f8e822c3c53b79939593a48
a <mishaal@dyn1228-244.wlan.ic.ac.uk>
2013-11-18 16:33:04 +0000

    Added print

--- src/backend/CodeGenerator.java
+++ src/backend/CodeGenerator.java
@@ -3,6 +3,8 @@ package backend;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.antlr.v4.runtime.tree.ParseTree;
+
 import antlr.BasicParser.Arg_listContext;
 import antlr.BasicParser.Array_literContext;
 import antlr.BasicParser.Array_typeContext;
@@ -42,6 +44,10 @@ public class CodeGenerator extends BasicParserBaseVisitor<String>{
 	private boolean[] freeRegs;
 	private int totalSize;
 	private Map<String, String> variables;
+	private int msgIndex;
+	private boolean[] message;
+	private boolean[] print;
+	private int stringIndex;
 	
 	private final int NUM_OF_REGS = 12;
 	private final String RESULT_REG = "r0";
@@ -63,6 +69,17 @@ public class CodeGenerator extends BasicParserBaseVisitor<String>{
 		addNewLabel("main");
 		addNewLabel("data");
 		stringLabelIndex = 0;
+		
+		msgIndex = 0;
+		stringIndex = 0;
+		
+		message = new boolean[10];
+		print = new boolean[10];
+		
+		for (int i = 0; i < message.length; i++) {
+			message[i] = true;
+			print[i] = true;
+		}
 	}
 	
 	private void setRegs() {
@@ -98,13 +115,15 @@ public class CodeGenerator extends BasicParserBaseVisitor<String>{
 	
 	private String getFreeReg() {
 		int freeReg = 0;
-		for(int i = 0; i < freeRegs.length; i++) {
-			if(freeRegs[i] == true){
+		
+		for (int i = 0; i < freeRegs.length; i++) {
+			if (freeRegs[i] == true) {
 				freeReg = i;
 				freeRegs[i] = false;
 				break;
 			}
 		}
+		
 		return "r" + (freeReg);
 	}
 	
@@ -147,6 +166,11 @@ public class CodeGenerator extends BasicParserBaseVisitor<String>{
 		output.put(currLabel, output.get(currLabel) + toAdd);
 	}
 	
+	private void addCMP(String strA, String strB) {
+		String toAdd = "\tCMP " + strA + ", " + strB + "\n";
+		output.put(currLabel, output.get(currLabel) + toAdd);
+	}
+	
 	private void addLDR(String strA,String strB) {
 		String toAdd = "\tLDR " + strA + ", " + strB + "\n";
 		output.put(currLabel, output.get(currLabel) + toAdd);
@@ -179,6 +203,16 @@ public class CodeGenerator extends BasicParserBaseVisitor<String>{
 	
 	private void addADD(String strA, String strB, String strC){
 		String toAdd = "\tADD " + strA + ", " + strB + ", " + strC + "\n";
+		output.put(currLabel, output.get(currLabel) + toAdd);
+	}
+	
+	private void addLDRNE(String strA, String strB) {
+		String toAdd = "\tLDRNE " + strA + ", " + strB + "\n";
+		output.put(currLabel, output.get(currLabel) + toAdd);
+	}
+	
+	private void addLDREQ(String strA, String strB) {
+		String toAdd = "\tLDREQ " + strA + ", " + strB + "\n";
 		output.put(currLabel, output.get(currLabel) + toAdd);
 	}
 	
@@ -292,13 +326,196 @@ public class CodeGenerator extends BasicParserBaseVisitor<String>{
 		return null;
 	}
 
-	public void addPrint(String s) {
+	public void addPrint(ParseTree expr) {
 		prevLabel = currLabel;
 		currLabel = "data";
-		addLabel("msg_" + stringLabelIndex++);
-		addLine(".word " + (s.length() - 1));
-		addLine(".ascii  " + s);
+		
+		if (expr instanceof Int_literContext) {
+			addPrint_Int((Int_literContext) expr);
+		}
+		else if (expr instanceof Bool_literContext) {
+			addPrint_Bool((Bool_literContext) expr);
+		}
+		else if (expr instanceof Char_literContext) {
+			addPrint_Char((Char_literContext) expr);
+		}
+		else if (expr instanceof Str_literContext) {
+			addPrint_String((Str_literContext) expr);
+		}
+		else if (expr instanceof Pair_literContext) {
+			addPrint_Pair((Pair_literContext) expr);
+		}
+		else if (expr instanceof Array_literContext) {
+			addPrint_Array((Array_literContext) expr);
+		}
+		else if (expr instanceof IdentContext) {
+			addPrint_Ident((IdentContext) expr);
+		}
+		
 		currLabel = prevLabel;
+	}
+	
+	private void addPrint_Int(Int_literContext ctx) {
+		if (message[PrintType.INT.ordinal()]) {
+			// Add message label
+			addLabel("msg_" + msgIndex++);
+			addLine(".word 3");
+			addLine(".ascii  \"%d\\0\"");
+			message[PrintType.INT.ordinal()] = false;
+		}
+				
+		String reg1 = getFreeReg();
+		int reg1_n = Integer.parseInt(reg1.substring(1));
+		
+		currLabel = "main";
+		addLDR(reg1, "=" + ctx.getText());
+		addBL("p_print_int");
+		
+		if (print[PrintType.INT.ordinal()]) {
+			String reg2 = getFreeReg();
+			int reg2_n = Integer.parseInt(reg2.substring(1));
+
+			// Create print function
+			addNewLabel("p_print_int");
+			currLabel = "p_print_int";
+			addLabel("p_print_int");
+			addPUSH("lr");
+			addMOV(reg2, reg1);
+			addLDR(reg1, "msg_" + (msgIndex - 1));
+			addADD(reg1, reg1, "#4");
+			addBL("printf");
+			addMOV(reg1, "#0");
+			addBL("fflush");
+			addPOP("pc");
+			
+			// Free registers
+			freeRegs[reg2_n] = true;
+			
+			print[PrintType.INT.ordinal()] = false;
+		}
+		
+		freeRegs[reg1_n] = true;
+	}
+	
+	private void addPrint_Bool(Bool_literContext ctx) {		
+		if (message[PrintType.BOOLEAN.ordinal()]) {
+			// Add boolean message labels
+			addLabel("msg_" + msgIndex++);
+			addLine(".word 5");
+			addLine(".ascii  \"true\\0\"");
+			addLabel("msg_" + msgIndex++);
+			addLine(".word 6");
+			addLine(".ascii  \"false\\0\"");
+			message[PrintType.BOOLEAN.ordinal()] = false;
+		}
+		
+		String reg1 = getFreeReg();
+		int reg1_n = Integer.parseInt(reg1.substring(1));
+
+		currLabel = "main";
+		
+		if (ctx.getText().equals("true")) {
+			addMOV(reg1, "#1");
+		} else {
+			addMOV(reg1, "#0");
+		}
+		
+		addBL("p_print_bool");
+		
+		if (print[PrintType.BOOLEAN.ordinal()]) {
+			addCMP(reg1, "#0");
+			addLDRNE(reg1, "=msg_" + (msgIndex - 2));
+			addLDREQ(reg1, "=msg_" + (msgIndex - 1));
+			addADD(reg1, reg1, "#4");
+			addBL("printf");
+			print[PrintType.BOOLEAN.ordinal()] = false;
+		}
+		
+		freeRegs[reg1_n] = true;
+	}
+	
+	private void addPrint_Char(Char_literContext ctx) {
+		String reg1 = getFreeReg();
+		int reg1_n = Integer.parseInt(reg1.substring(1));
+		currLabel = "main";
+		
+		addMOV(reg1, "#" + ctx.getText());
+		addBL("putchar");
+		addMOV(reg1, "#0");
+		
+		freeRegs[reg1_n] = true;		
+	}
+	
+	private void addPrint_String(Str_literContext ctx) {
+		if (message[PrintType.STRING.ordinal()]) {
+			stringIndex = msgIndex;
+			addLabel("msg_" + msgIndex++);
+			addLine(".word 5");
+			addLine(".ascii  \"%.*s\0\"");
+			message[PrintType.STRING.ordinal()] = false;
+		}
+		
+		// Add msg label
+		String s = ctx.getText();
+		addLabel("msg_" + msgIndex++);
+		addLine(".word " + (s.length() - 2));
+		addLine(".ascii  " + s);
+		
+		// Get register
+		String reg1 = getFreeReg();
+		int reg1_n = Integer.parseInt(reg1.substring(1));
+
+		// Add main instructions
+		currLabel = "main";
+		addLDR(reg1, "=msg_" + (msgIndex - 1));
+		addBL("p_print_string");
+		
+		// Add print function
+		if (print[PrintType.STRING.ordinal()]) {
+			addNewLabel("p_print_string");
+			currLabel = "p_print_string";
+			addLabel("p_print_string");
+			
+			String reg2 = getFreeReg();
+			int reg2_n = Integer.parseInt(reg2.substring(1));
+			String reg3 = getFreeReg();
+			int reg3_n = Integer.parseInt(reg2.substring(1));
+			
+			addPUSH("lr");
+			addLDR(reg2, "[" + reg1 + "]");
+			addADD(reg3, reg1, "#4");
+			addLDR(reg1, "=msg_" + stringIndex);
+			addADD(reg1, reg1, "#4");
+			addBL("printf");
+			addMOV(reg1, "#0");
+			addBL("fflush");
+			addPOP("pc");
+			
+			// Free registers
+			freeRegs[reg2_n] = true;		
+			freeRegs[reg3_n] = true;		
+			
+			print[PrintType.STRING.ordinal()] = false;
+		}
+		
+		freeRegs[reg1_n] = true;		
+	}
+	
+	private void addPrint_Pair(Pair_literContext ctx) {
+		
+	}
+	
+	private void addPrint_Array(Array_literContext ctx) {
+		
+	}
+	
+	private void addPrint_Ident(IdentContext ctx) {
+		
+		
+	}
+	
+	public void addPrintln() {
+		
 	}
 	
 	@Override
@@ -346,19 +563,25 @@ public class CodeGenerator extends BasicParserBaseVisitor<String>{
 			addBL("exit");
 		}
 		else if (ctx.getChild(0).getText().equals("print") || ctx.getChild(0).getText().equals("println")) {
-			addPrint(ctx.getChild(1).getText());
-		}	
-	/* else if (ctx.getChild(0) instanceof BasicParser.StatContext) {
-	for (int i = 0 ; i < ctx.getChildCount() ; i += 2) {
-		if (ctx.getChild(i).getChild(0) instanceof BasicParser.TypeContext) {
-			totalSize += getSize(ctx.getChild(i).getChild(0).getText());
+			addPrint(ctx.getChild(1).getChild(0));
+			
+			if (ctx.getChild(0).getText().equals("println")) {
+				addPrintln();
+			}		}	
+		/* 
+		
+		else if (ctx.getChild(0) instanceof BasicParser.StatContext) {
+		for (int i = 0 ; i < ctx.getChildCount() ; i += 2) {
+			if (ctx.getChild(i).getChild(0) instanceof BasicParser.TypeContext) {
+				totalSize += getSize(ctx.getChild(i).getChild(0).getText());
+			}
 		}
+		addSUB(STACK_POINTER, STACK_POINTER, "#" + totalSize);
+		super.visitStat(ctx);
+		addADD(STACK_POINTER, STACK_POINTER, "#" + totalSize);
+		
 	}
-	addSUB(STACK_POINTER, STACK_POINTER, "#" + totalSize);
-	super.visitStat(ctx);
-	addADD(STACK_POINTER, STACK_POINTER, "#" + totalSize);
-	
-}*/
+	*/
 		return super.visitStat(ctx);
 	}
 
